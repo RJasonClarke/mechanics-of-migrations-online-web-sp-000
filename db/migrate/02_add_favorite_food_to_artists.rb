b00b4cb0813370c543ad991571a6db8082692201
Robert Clarke <r.jason.clarke@gmail.com>
2021-06-10 17:19:18 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,5 +1,5 @@
-class AddGenderToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change
     add_column :artists, :gender, :string
   end
-end 
+end
